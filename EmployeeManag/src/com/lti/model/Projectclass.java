c8800f5d6342d761a5eb9cbfd4ae9ebef9e8a91a
Santhosh <Santhosh@Santhosh>
2020-10-15 13:12:29 +0530

    added projectclass

---
+++ EmployeeManag/src/com/lti/model/Projectclass.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Projectclass {
+   int ProjectCode;
+   String ProjectName;
+public int getProjectCode() {
+	return ProjectCode;
+}
+public void setProjectCode(int projectCode) {
+	ProjectCode = projectCode;
+}
+public String getProjectName() {
+	return ProjectName;
+}
+public void setProjectName(String projectName) {
+	ProjectName = projectName;
+}
+
+}
